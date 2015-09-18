7a2f635136239e8d7ebaa87fe899ceaaeee65f56
elkapalkaproduction <elkapalkaproduction@gmail.com>
2015-09-18 16:34:17 -0700

    Cosmetic Change
    added the last 3 columns

--- Daily_Salary_Variances.py
+++ Daily_Salary_Variances.py
@@ -13,7 +13,14 @@ This script will look attempt to do the following
     
 2.  It will then look for specific CVS file named "DKS...", the program will then pick specific columns from this file and the internal list from #1 and write it to a new file
     A.  First and Last Name of the player - will only write it if it's find a corresponding name from FanDuel Data
-    B.  dddggg
+    B.  Position of the player (First Column of the DKS... file)
+    C.  DK Pts - 5th column of the DKS... file
+    D.  DK Price - 3rd column of the DKS file
+    E.  FD Points - from the internal list
+    F.  FD Price - from the internal list
+    G.  DK Price % - calculated field, takes the DK price and divides it by 50,000 - budget for DK
+    H.  FD Price % - calculated field, takes the FD price and divides it by 60,000 - budget for FD
+    I.  Diff DK-FD = DK Price% - FD Price%
 
 '''
 from _ssl import err_codes_to_names
@@ -92,7 +99,7 @@ if __name__ == '__main__':
                     if headers:
                         #writerTemp.writerow(headers)
                         # headers.insert(0,'Name ID')
-                        writerMain.writerow(('Name','POS','DK Pts','DK Price','FD Points', 'FD Price', 'DK/FD Pts%', 'DK/FD Price %'))
+                        writerMain.writerow(('Name','POS','DK Pts','DK Price','FD Points', 'FD Price', 'DK Price %', 'FD Price %', 'Diff DK/FD'))
                     for row in reader:
                         # Skip DEFENSES for Now and change some of the player's names since they are different in FanDule and DK
                         if row[0]<>'DST' and row[1] not in NotPlaying:
@@ -114,7 +121,12 @@ if __name__ == '__main__':
                             for t in range(len(FanDuelData)):
                                 if FanDuelData[t][0]==s:
                                     row.insert(0,s)
-                                    writerMain.writerow((row[0],row[1],row[5],row[3],FanDuelData[t][1],FanDuelData[t][2]))
+
+                                    calcDK = float(row[3])/50000
+                                    caldFD = float(FanDuelData[t][2])/60000
+                                    diff = calcDK-caldFD
+                                   
+                                    writerMain.writerow((row[0],row[1],row[5],row[3],FanDuelData[t][1],FanDuelData[t][2],calcDK,caldFD,diff))
                                     break
                     try:
                         inp.close()
@@ -136,12 +148,14 @@ if __name__ == '__main__':
                 
     dirs = os.listdir(path)
     os.chdir(path)
-    
-    # rename TXT to CVS
+                 
+            
+    # rename Results.csv into Excel
     for file in dirs:
         if file[len(file)-3:len(file)]=='txt':
             base = os.path.splitext(file)[0]
             os.rename(file, base+ ".csv")
+           
             
     dirs = os.listdir(path)
     os.chdir(path)
